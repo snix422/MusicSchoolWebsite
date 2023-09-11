47d76524342f0e85043f3cec35569e863ad4da55
snix422 <102601543+snix422@users.noreply.github.com>
2023-09-11 22:36:56 +0200

    fixed offers component

--- my-app/src/components/Offers/Offers.tsx
+++ my-app/src/components/Offers/Offers.tsx
@@ -8,7 +8,7 @@ const Offers = () => {
     return (
         <div className="min-h-screen">
             <NavBar />
-            <div className="w-screen h-screen bg-yellow-900 flex flex-col items-center gap-28 max-sm:justify-center max-sm:gap-10 xl:pt-48 lg:pt-48 md:pt-28 sm:pt-28">
+            <div className="w-screen min-h-screen bg-yellow-900 flex flex-col items-center gap-28 pb-10 max-sm:justify-center max-sm:gap-10 xl:pt-48 lg:pt-48 md:pt-28 sm:pt-28">
                 <motion.h2 initial={{y:-100,opacity:0}} animate={{y:0,opacity:1}} transition={{delay:0.5, duration:1}} className="text-white text-3xl max-sm:pb-28" style={{fontFamily:'Montserrat'}}>Oferta edukacyjna</motion.h2>
                 <motion.div initial={{y:100,opacity:0}} animate={{y:0,opacity:1}} transition={{delay:0.5, duration:1}} className="w-screen flex flex-col items-center">
                 <Accordion className="w-3/5  max-md:w-4/5">
